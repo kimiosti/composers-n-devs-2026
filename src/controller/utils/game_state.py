2d699f30ba5d7084f4995b29876b31eee273d198
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-14 18:26:47 +0100

    Created enumerative type to track game states

---
+++ src/controller/utils/game_state.py
@@ -0,0 +1,12 @@
+"""Module for enumerating all possible game states."""
+
+from enum import Enum
+
+class GameState(Enum):
+    """Enumerative type listing all possible game states."""
+    MAIN_MENU = 0
+    GAME = 1
+    LEVEL_TRANSITION = 2
+    DIALOGUE = 3
+    PAUSE_MENU = 4
+    ENDING = 5
