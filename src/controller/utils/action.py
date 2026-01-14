9f1aa2bfa5693fe178178f3a7fd3128ad9dc80fb
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-14 19:25:14 +0100

    Added action enumerative type

---
+++ src/controller/utils/action.py
@@ -0,0 +1,14 @@
+"""Module enumerating all possible game actions."""
+
+from enum import Enum
+
+class Action(Enum):
+    """Enumerative type listing all possible game actions."""
+    QUIT = 0
+    MOVE_UP = 1
+    MOVE_DOWN = 2
+    MOVE_RIGHT = 3
+    MOVE_LEFT = 4
+    ENTER = 5
+    INTERACT = 6
+    # TODO - more actions for all dialogue options
