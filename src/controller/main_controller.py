776aaae2ef4c3f51244ff5b093377af6e44c05a8
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-16 17:12:57 +0100

    Modified main controller to check for button pression events rather than polling the pressure status for better user experience in menu navigation

--- src/controller/main_controller.py
+++ src/controller/main_controller.py
@@ -1,9 +1,13 @@
 """Module containing the main game controller."""
 
-import pygame
+from typing import List
+from pygame import KEYDOWN
+from pygame.key import ScancodeWrapper
+from pygame.event import Event
 from controller.utils.game_state import GameState
 from controller.utils.action import Action
 from model.menu.menu import Menu
+from view.menu.menu_view import MenuView
 from constant import KEY_MAPPINGS
 
 class MainController():
@@ -16,10 +20,11 @@ class MainController():
         self._world: "None" = None # TODO - instantiate game world.
         self._level_number: "int" = 0 # TODO - introduce level progression controller ?
 
-    def update(self, pressed_keys: "pygame.key.ScancodeWrapper", dt: "int") -> "bool":
+    def update(self, events: "List[Event]", pressed_keys: "ScancodeWrapper", dt: "int") -> "bool":
         """Performs a single model update step.
         
         Positional arguments:  
+         - `events`: a list of all events occurred since the last update step.
          - `pressed_keys`: all the keys that are currently being pressed.  
          - `dt`: the amount of time elapsed since last update step, in milliseconds.
         
@@ -28,29 +33,29 @@ class MainController():
         for key, action in KEY_MAPPINGS.items():
             if pressed_keys[key]:
                 match action:
-                    case Action.MOVE_UP:
-                        self._menu.move_up()
-                        # TODO - move inside game world
-                    case Action.MOVE_DOWN:
-                        self._menu.move_down()
-                        # TODO - move inside game world
-                    case Action.MOVE_RIGHT:
-                        # TODO - move inside game world
+                    case _:
+                        # TODO - handle in-world movement
                         pass
-                    case Action.MOVE_LEFT:
-                        # TODO - move inside game world
-                        pass
-                    case Action.INTERACT:
-                        self.change_state(self._menu.enter_selection())
-                        # TODO - handle interactions in the game world
-                    case Action.QUIT:
-                        match self._game_state:
-                            case GameState.GAME:
-                                self.change_state(GameState.PAUSE_MENU)
-                            case GameState.PAUSE_MENU:
-                                self.change_state(GameState.GAME)
-                            case GameState.MAIN_MENU:
-                                self.change_state(GameState.QUITTING)
+
+        for event in events:
+            if event.type == KEYDOWN:
+                try:
+                    match KEY_MAPPINGS[event.key]:
+                        case Action.MOVE_UP:
+                            self._menu.move_up()
+                        case Action.MOVE_DOWN:
+                            self._menu.move_down()
+                        case Action.INTERACT:
+                            self.change_state(self._menu.enter_selection())
+                        case Action.QUIT:
+                            self.change_state(
+                                GameState.PAUSE_MENU if self._game_state == GameState.GAME
+                                else GameState.GAME if self._game_state == GameState.PAUSE_MENU
+                                else GameState.QUITTING if self._game_state == GameState.MAIN_MENU
+                                else self._game_state
+                            )
+                except KeyError:
+                    pass
 
         #TODO - handle update step.
 
@@ -58,7 +63,9 @@ class MainController():
 
     def render_world(self) -> "None":
         """Renders the next frame."""
-        #TODO - handle rendering.
+        match self._game_state:
+            case menu_state if menu_state in [GameState.MAIN_MENU, GameState.PAUSE_MENU]:
+                MenuView().render_menu(self._menu.get_options(), self._menu.get_selected())
 
     def change_state(self, dest_state: GameState) -> "None":
         """Changes the game state.
