535ca20528f82c22b30de73dc8983a8114fa3352
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-16 10:22:15 +0100

    Added module for game constants

---
+++ src/constant.py
@@ -0,0 +1,14 @@
+"""Module containing all game constants."""
+
+from typing import Dict
+import pygame
+from controller.utils.action import Action
+
+KEY_MAPPINGS: "Dict[int, Action]" = {
+    pygame.K_ESCAPE: Action.QUIT,
+    pygame.K_UP: Action.MOVE_UP,
+    pygame.K_DOWN: Action.MOVE_DOWN,
+    pygame.K_RIGHT: Action.MOVE_RIGHT,
+    pygame.K_LEFT: Action.MOVE_LEFT,
+    pygame.K_x: Action.INTERACT
+}
