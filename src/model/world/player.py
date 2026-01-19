46a17fe5c43570ff878da454c2cdb97a5b6436fe
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-19 17:39:14 +0100

    Removed redundant constructor declaration, fixed move method and fixed linting on lines too long

--- src/model/world/player.py
+++ src/model/world/player.py
@@ -9,18 +9,6 @@ from model.world.entity import Entity
 class Player(Entity):
     """Implementation for the player-controlled game entities."""
 
-    def __init__(self, pos: "Tuple[float, float]", size: "Tuple[float, float]",
-                 name: "str", direction: "Direction") -> "None":
-        """Instantiates a player-controlled game entity.
-        
-        Positional arguments:  
-         - `pos`: the coordinates of the top-left corner of the player's hitbox \
-            when spawned.  
-         - `size`: the dimension of the player's hitbox.  
-         - `name`: the player's name, useful for retrieving the proper texture.  
-         - `direction`: the direction towards which the player is facing when spawned."""
-        super().__init__(pos, size, name, direction)
-
     def move(self, direction: "Direction", dt: "int") -> "Rect":
         """Computes the player's movement in the desired direction.
         
@@ -31,14 +19,15 @@ class Player(Entity):
         Return:  
          - The position the hitbox would occupy after its movement.
          
-        Note that the game world coordinate system is left-handed. The origin is the top-left corner \
-            of the map, with x increasing towards the right and y increasing towards the bottom."""
+        Note that the game world coordinate system is left-handed. The origin is the top-left \
+        corner of the map, with x increasing towards the right and y increasing towards the \
+        bottom."""
         self._direction = direction
         return self._hitbox.move(
-            PLAYER_SPEED * dt if direction == Direction.RIGHT else
-            - PLAYER_SPEED * dt if direction == Direction.LEFT else 0,
-            PLAYER_SPEED * dt if direction == Direction.DOWN else
-            - PLAYER_SPEED * dt if direction == Direction.UP else 0
+            PLAYER_SPEED * dt / 1000 if direction == Direction.RIGHT else
+            - PLAYER_SPEED * dt / 1000 if direction == Direction.LEFT else 0,
+            PLAYER_SPEED * dt / 1000 if direction == Direction.DOWN else
+            - PLAYER_SPEED * dt / 1000 if direction == Direction.UP else 0
         )
 
     def move_to(self, pos: "Tuple[float, float]") -> "None":
