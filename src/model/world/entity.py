910a01f60a932afbe6ff36677df1b29d6bac443b
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-19 17:36:13 +0100

    Added support for direction for better code reusability

--- src/model/world/entity.py
+++ src/model/world/entity.py
@@ -2,24 +2,27 @@
 
 from typing import Tuple
 from pygame import Rect
+from model.world.direction import Direction
 from constant import VISUAL_ASSETS_FOLDER, CHARACTER_ASSETS_FOLDER, ANIMATION_DEPTH, \
-    VISUAL_ASSETS_EXTENSION
+    VISUAL_ASSETS_EXTENSION, get_direction_asset_postfix
 
 class Entity():
     """Implementation for all non-playing game entities."""
 
     def __init__(self, pos: "Tuple[float, float]", size: "Tuple[float, float]",
-                 name: "str") -> "None":
+                 name: "str", direction: "Direction") -> "None":
         """Instantiates a non-playing game entity.
         
         Positional arguments:  
          - `pos`: the coordinates of the top-left corner of the entity's hitbox \
             when spawned.  
          - `size`: the dimension of the entity's hitbox.  
-         - `name`: the entity's name, useful for retrieving the proper visual asset."""
+         - `name`: the entity's name, useful for retrieving the proper visual asset.
+         - `direction`: the direction towards which the entity is facing."""
         self._hitbox: "Rect" = Rect(pos, size)
-        self._asset_base_name: "str" = VISUAL_ASSETS_FOLDER + CHARACTER_ASSETS_FOLDER + name
+        self._name: "str" = name
         self._frame_num: "int" = 0
+        self._direction = direction
 
     def is_colliding(self, hitbox: "Rect") -> "bool":
         """Checks for collision with a given hitbox.
@@ -37,4 +40,6 @@ class Entity():
         Return:  
          - A string identifier for the next proper resource to be shown on screen."""
         self._frame_num = self._frame_num + 1 % ANIMATION_DEPTH
-        return self._asset_base_name + str(self._frame_num) + VISUAL_ASSETS_EXTENSION
+        return VISUAL_ASSETS_FOLDER + CHARACTER_ASSETS_FOLDER + self._name \
+            + get_direction_asset_postfix(self._direction) + str(self._frame_num) \
+            + VISUAL_ASSETS_EXTENSION
