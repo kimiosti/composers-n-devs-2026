ff1d53280d6d344561cfb80e077039ceebbf057b
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-19 17:36:41 +0100

    Added player implementation

---
+++ src/model/world/player.py
@@ -0,0 +1,49 @@
+"""Module containing the implementation for the player-controlled game entities."""
+
+from typing import Tuple
+from pygame import Rect
+from constant import PLAYER_SPEED
+from model.world.direction import Direction
+from model.world.entity import Entity
+
+class Player(Entity):
+    """Implementation for the player-controlled game entities."""
+
+    def __init__(self, pos: "Tuple[float, float]", size: "Tuple[float, float]",
+                 name: "str", direction: "Direction") -> "None":
+        """Instantiates a player-controlled game entity.
+        
+        Positional arguments:  
+         - `pos`: the coordinates of the top-left corner of the player's hitbox \
+            when spawned.  
+         - `size`: the dimension of the player's hitbox.  
+         - `name`: the player's name, useful for retrieving the proper texture.  
+         - `direction`: the direction towards which the player is facing when spawned."""
+        super().__init__(pos, size, name, direction)
+
+    def move(self, direction: "Direction", dt: "int") -> "Rect":
+        """Computes the player's movement in the desired direction.
+        
+        Positional arguments:  
+         - `direction`: the direction towards where the player should move.  
+         - `dt`: the amount of time elapsed since the last world update step, in milliseconds.
+        
+        Return:  
+         - The position the hitbox would occupy after its movement.
+         
+        Note that the game world coordinate system is left-handed. The origin is the top-left corner \
+            of the map, with x increasing towards the right and y increasing towards the bottom."""
+        self._direction = direction
+        return self._hitbox.move(
+            PLAYER_SPEED * dt if direction == Direction.RIGHT else
+            - PLAYER_SPEED * dt if direction == Direction.LEFT else 0,
+            PLAYER_SPEED * dt if direction == Direction.DOWN else
+            - PLAYER_SPEED * dt if direction == Direction.UP else 0
+        )
+
+    def move_to(self, pos: "Tuple[float, float]") -> "None":
+        """Moves the player to a given position.
+        
+        Positional arguments:  
+         - `pos`: the position where the player should be moved."""
+        self._hitbox = Rect(pos, self._hitbox.size)
