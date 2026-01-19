d374edb38940283e0ca598277d19cc4db3012181
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-19 17:35:38 +0100

    Added utility function to compute asset name postfix given a movement direction and constant for player-controlled entities movement speed

--- src/constant.py
+++ src/constant.py
@@ -3,6 +3,7 @@
 from typing import Dict
 import pygame
 from controller.utils.action import Action
+from model.world.direction import Direction
 
 KEY_MAPPINGS: "Dict[int, Action]" = {
     pygame.K_ESCAPE: Action.QUIT,
@@ -13,6 +14,8 @@ KEY_MAPPINGS: "Dict[int, Action]" = {
     pygame.K_x: Action.INTERACT
 }
 
+PLAYER_SPEED: "float" = 10
+
 VISUAL_ASSETS_FOLDER: "str" = "resources/assets/"
 
 VISUAL_ASSETS_EXTENSION: "str" = ".png"
@@ -20,3 +23,13 @@ VISUAL_ASSETS_EXTENSION: "str" = ".png"
 CHARACTER_ASSETS_FOLDER: "str" = "characters/"
 
 ANIMATION_DEPTH: "int" = 2
+
+def get_direction_asset_postfix(direction: "Direction") -> "str":
+    """Computes the asset postfix given the entity's movement direction.
+    
+    Positional arguments:  
+     - `direction`: the direction of the entity's movement.
+    
+    Return:  
+     - A string postfix to be added to the asset name."""
+    return f"_{direction.name}_"
