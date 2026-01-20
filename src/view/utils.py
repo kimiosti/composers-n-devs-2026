05ad52be1d8cc3fe81d38616d9fd095c41ed3477
Kimi Osti <kimi.osti@studio.unibo.it>
2026-01-20 01:07:02 +0100

    Added module for better reuse of view utility code

---
+++ src/view/utils.py
@@ -0,0 +1,18 @@
+"""Module containing generic view utilities."""
+
+from pygame import display as pg_display
+from pygame.surface import Surface
+
+def get_clear_display() -> "Surface":
+    """Initializes the screen (if necessary), cleans up the previous frame \
+        and returns the current display surface.
+        
+    Return:  
+     - A surface representing the current display."""
+    display: "Surface" = pg_display.get_surface()
+    if display is None:
+        display = pg_display.set_mode()
+
+    display.fill((0,0,0))
+
+    return display
